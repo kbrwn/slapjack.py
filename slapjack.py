94197398c0005eb8c946ec445dd1bceb544f79a6
Kyle Brown <kyle.steven.brown@gmail.com>
2014-09-12 10:21:12 -0500

    function to print cards added

--- slapjack.py
+++ slapjack.py
@@ -1,6 +1,7 @@
-import random
+import random, time
 
-deck = {}
+
+deck = {} #this is a dictionary of 0-51. Each value has two keys.
 
 faces = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six',\
   'Seven', 'Eight', 'Nine', 'Ten', 'Jack',\
@@ -8,19 +9,28 @@ faces = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six',\
 
 suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts']
 
+
+# Function to initiliaze the deck of cards.
+#
+
 def initdeck():
-    #x = deck.get()
     for i in range(52):
         card_face = random.choice(faces)
         card_suit = random.choice(suits)
-        deck[0] = (card_suit,card_face)
+        deck[0] = (card_suit,card_face) # initial card to be checked against
 
         if (card_suit,card_face) in deck:
             break
         else:
             deck[i] = (card_suit,card_face)
 
-
-    print 'deck =', deck
+def printdeck():
+    for i in range(52):
+        card = deck.get(i)
+        print card[1] + ' of ' + card[0]
+        time.sleep(1)
 
 initdeck()
+printdeck()
+
+#print 'deck =', deck
