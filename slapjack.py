c4528fe22bca066e7e7e92e3386f333dabe3a1e5
Kyle Brown <kyle.steven.brown@gmail.com>
2014-09-21 16:03:36 -0500

    more logic, more loops

--- slapjack.py
+++ slapjack.py
@@ -1,4 +1,18 @@
-import random, time
+import random, time, thread, threading
+
+'''
+this game is a work in progress - 9/21/2014
+
+slapjack.py
+
+to do:
+
+ - fix loop to prevent duplicate cards
+ - thread stop printing cards
+ - develop a system to count score
+
+
+'''
 
 
 deck = {} #this is a dictionary of 0-51. Each value has two keys.
@@ -10,27 +24,37 @@ faces = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six',\
 suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts']
 
 
-# Function to initiliaze the deck of cards.
-#
+def input_thread(L):
+#function to use in the thread that waits for input
+    raw_input()
+    L.append(None) # list that will be appended if player hits a key ()"slaping")
+
 
 def initdeck():
+#function to initiliaze the deck of cards
     for i in range(52):
         card_face = random.choice(faces)
         card_suit = random.choice(suits)
         deck[0] = (card_suit,card_face) # initial card to be checked against
 
-        if (card_suit,card_face) in deck:
+        if (card_suit,card_face) in deck: # this loop is meant to check for duplicate cards and not print them. Currently not working.
             break
         else:
             deck[i] = (card_suit,card_face)
 
 def printdeck():
-    for i in range(52):
-        card = deck.get(i)
-        print card[1] + ' of ' + card[0]
-        time.sleep(1)
+#function to print deck
+    L = []
+    thread.start_new_thread(input_thread, (L,)) #this thread is waiting for user input
+    while True:
+        for i in range(52):
+            card = deck.get(i) #sudden realization that I should probably use some loop logic to prevent duplicates from being printed
+            print card[1] + ' of ' + card[0]
+            time.sleep(1)
+            if L:
+                print "You Slapped!"
+                L.pop()
+
 
 initdeck()
 printdeck()
-
-#print 'deck =', deck
